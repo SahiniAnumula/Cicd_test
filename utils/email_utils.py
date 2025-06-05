55cfad19f283ca45395cb3274776290ee3d2857d
SahiniAnumula <sahinianumula20@gmail.com>
2025-06-05 12:39:34 +0530

    set ses email

---
+++ utils/email_utils.py
@@ -0,0 +1,17 @@
+import boto3
+
+def send_ses_email(to_email, subject, body):
+    ses_client = boto3.client('ses', region_name='eu-west-1')  # Europe region
+    
+
+    response = ses_client.send_email(
+        Source='ahtidev@gmail.com',  # Must be verified in SES
+        Destination={'ToAddresses': [to_email]},
+        Message={
+            'Subject': {'Data': subject},
+            'Body': {
+                'Text': {'Data': body}
+            }
+        }
+    )
+    return response
