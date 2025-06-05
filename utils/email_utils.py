7c883746b3bb99f746917136cf7e291b96823226
vedithathanneru <161914920+vedithathanneru@users.noreply.github.com>
2025-06-05 15:08:53 +0530

    Update email_utils.py

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -1,7 +1,7 @@
 import boto3
 
 def send_ses_email(to_email, subject, body):
-    ses_client = boto3.client('ses', region_name='eu-west-1')  # Europe region
+    ses_client = boto3.client('ses', region_name='us-east-1')  # Europe region
     
 
     response = ses_client.send_email(
